a600d05758ebac19b7b7880a07713a5199960bbe
Hilmy Thoriq <hilmythoriq09@gmail.com>
2022-06-05 19:28:31 +0700

    add all ball

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ from math import *
 import pygame
 import sys
 
+pygame.init()
 pygame.display.set_caption("PyPool")
 display = pygame.display.set_mode((WIDTH, OUTER_HEIGHT))
+clock = pygame.time.Clock()
 
 class Ball:
     def __init__(self, x, y, speed, color, angle, ballNum):
@@ -15,6 +17,7 @@ class Ball:
         self.angle = angle
         self.speed = speed
         self.ballNum = ballNum
+        self.font = pygame.font.SysFont("Agency FB", 10)
 
     def draw(self, x, y):
         pygame.draw.ellipse(display, self.color, (x - RADIUS, y - RADIUS, RADIUS*2, RADIUS*2))
@@ -46,42 +49,42 @@ def border():
     pygame.draw.rect(display, BORDER_COLOR, (0, HEIGHT - 30, WIDTH, HEIGHT))
 
 def initial_state():
-    global BALLS
-    BALLS = []
+    global balls
+    balls = []
 
     s = 70
 
-    b1 = Ball(s, HEIGHT/2 - 4*RADIUS, 0, BLACK, 0, 1)
-    b2 = Ball(s + 2*RADIUS, HEIGHT/2 - 3*RADIUS, 0, WHITE, 0, 2)
-    b3 = Ball(s, HEIGHT/2 - 2*RADIUS, 0, BLACK, 0, 3)
-    b4 = Ball(s + 4*RADIUS, HEIGHT/2 - 2*RADIUS, 0, WHITE, 0, 4)
-    b5 = Ball(s + 2*RADIUS, HEIGHT/2 - 1*RADIUS, 0, BLACK, 0, 5)
-    b6 = Ball(s, HEIGHT/2, 0, WHITE, 0, 6)
-    b7 = Ball(s + 6*RADIUS, HEIGHT/2 - 1*RADIUS, 0, BLACK, 0, 7)
-    b8 = Ball(s + 4*RADIUS, HEIGHT/2, 0, WHITE, 0, 8)
-    b9 = Ball(s + 8*RADIUS, HEIGHT/2, 0, BLACK, 0, 9)
-    b10 = Ball(s + 6*RADIUS, HEIGHT/2 + 1*RADIUS, 0, WHITE, 0, 10)
-    b11 = Ball(s + 2*RADIUS, HEIGHT/2 + 1*RADIUS, 0, BLACK, 0, 11)
-    b12 = Ball(s, HEIGHT/2 + 2*RADIUS, 0, WHITE, 0, 12)
-    b13 = Ball(s + 4*RADIUS, HEIGHT/2 + 2*RADIUS, 0, BLACK, 0, 13)
-    b14 = Ball(s + 2*RADIUS, HEIGHT/2 + 3*RADIUS, 0, WHITE, 0, 14)
-    b15 = Ball(s, HEIGHT/2 + 4*RADIUS, 0, BLACK, 0, 15)
+    b1 = Ball(s, HEIGHT/2 - 4*RADIUS, 0, colors[0], 0, 1)
+    b2 = Ball(s + 2*RADIUS, HEIGHT/2 - 3*RADIUS, 0, colors[1], 0, 2)
+    b3 = Ball(s, HEIGHT/2 - 2*RADIUS, 0, colors[2], 0, 3)
+    b4 = Ball(s + 4*RADIUS, HEIGHT/2 - 2*RADIUS, 0, colors[3], 0, 4)
+    b5 = Ball(s + 2*RADIUS, HEIGHT/2 - 1*RADIUS, 0, colors[4], 0, 5)
+    b6 = Ball(s, HEIGHT/2, 0, colors[5], 0, 6)
+    b7 = Ball(s + 6*RADIUS, HEIGHT/2 - 1*RADIUS, 0, colors[6], 0, 7)
+    b8 = Ball(s + 4*RADIUS, HEIGHT/2, 0, colors[7], 0, 8)
+    b9 = Ball(s + 8*RADIUS, HEIGHT/2, 0, colors[8], 0, 9)
+    b10 = Ball(s + 6*RADIUS, HEIGHT/2 + 1*RADIUS, 0, colors[9], 0, 10)
+    b11 = Ball(s + 2*RADIUS, HEIGHT/2 + 1*RADIUS, 0, colors[10], 0, 11)
+    b12 = Ball(s, HEIGHT/2 + 2*RADIUS, 0, colors[11], 0, 12)
+    b13 = Ball(s + 4*RADIUS, HEIGHT/2 + 2*RADIUS, 0, colors[12], 0, 13)
+    b14 = Ball(s + 2*RADIUS, HEIGHT/2 + 3*RADIUS, 0, colors[13], 0, 14)
+    b15 = Ball(s, HEIGHT/2 + 4*RADIUS, 0, colors[14], 0, 15)
 
-    BALLS.append(b1)
-    BALLS.append(b2)
-    BALLS.append(b3)
-    BALLS.append(b4)
-    BALLS.append(b5)
-    BALLS.append(b6)
-    BALLS.append(b7)
-    BALLS.append(b8)
-    BALLS.append(b9)
-    BALLS.append(b10)
-    BALLS.append(b11)
-    BALLS.append(b12)
-    BALLS.append(b13)
-    BALLS.append(b14)
-    BALLS.append(b15)
+    balls.append(b1)
+    balls.append(b2)
+    balls.append(b3)
+    balls.append(b4)
+    balls.append(b5)
+    balls.append(b6)
+    balls.append(b7)
+    balls.append(b8)
+    balls.append(b9)
+    balls.append(b10)
+    balls.append(b11)
+    balls.append(b12)
+    balls.append(b13)
+    balls.append(b14)
+    balls.append(b15)
 
 def scoreBar():
     pygame.draw.rect(display, (51, 51, 51), (0, HEIGHT, WIDTH, OUTER_HEIGHT))
@@ -93,6 +96,7 @@ def close():
 # Main Function
 def Table():
     loop = True
+    initial_state()
 
     noHoles = 6
     holes = []
@@ -111,6 +115,8 @@ def Table():
     holes.append(h5)
     holes.append(h6)
 
+    cueBall = Ball(WIDTH/2, HEIGHT/2, 0, WHITE, 0, "cue")
+
     while loop:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -125,8 +131,10 @@ def Table():
 
         display.fill(BACK_COLOR)
 
-        for i in range(len(BALLS)):
-            BALLS[i].draw(BALLS[i].x, BALLS[i].y)
+        for i in range(len(balls)):
+            balls[i].draw(balls[i].x, balls[i].y)
+
+        cueBall.draw(cueBall.x, cueBall.y)
 
         border()
 
